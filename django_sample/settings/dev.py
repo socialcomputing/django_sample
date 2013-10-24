8d72623cd8bcc4a7485f48da82749953f3b416fc
Roman Prokofyev <roman.prokofyev@gmail.com>
2013-10-24 14:01:18 +0200

    initial commit

---
+++ django_sample/settings/dev.py
@@ -0,0 +1,39 @@
+from .base import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'dev.db',
+        }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-english-learners',
+        'OPTIONS': {
+            'MAX_ENTRIES': 100000
+        }
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+INTERNAL_IPS = ('',)
+# set up Django Debug Toolbar if installed
+try:
+    import debug_toolbar
+    MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware', )
+    INSTALLED_APPS += ('debug_toolbar', )
+    DEBUG_TOOLBAR_CONFIG = {
+        'INTERCEPT_REDIRECTS': False,
+        'SHOW_TOOLBAR_CALLBACK': lambda *args, **kwargs: True,
+        'SQL_COUNT_THRESHOLD': 100,
+    }
+except ImportError:
+    pass
+
+ALLOWED_HOSTS = ['*']
